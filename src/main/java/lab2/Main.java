ca940374ac876509f47e918db992aace9ba71605
Ilya.Minochkin <Ilya.Minochkin@yandex.ru>
2021-10-14 14:37:57 +0500

    Initial commit

---
+++ src/main/java/lab2/Main.java
@@ -0,0 +1,14 @@
+package lab2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+		
+	}
+}
